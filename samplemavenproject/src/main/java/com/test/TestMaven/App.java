3d87908a4d28ccf484946cd86553bce61d7c3518
Sanjeev_Sharma04 <sanjeev_sharma04@infosys.com>
2018-12-05 12:41:45 +0530

    Done local changes

--- samplemavenproject/src/main/java/com/test/TestMaven/App.java
+++ samplemavenproject/src/main/java/com/test/TestMaven/App.java
@@ -8,6 +8,10 @@ public class App
 {
     public static void main( String[] args )
     {
+<<<<<<< Upstream, based on origin/master
         System.out.println( "--> :: --> Hello World!!! " );
+=======
+        System.out.println( "--> Hello World!!! " );
+>>>>>>> 6659188 Done local changes
     }
 }
